dbaa7d030466cfc10c431ac14765785055d3c3a0
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-11-01 12:52:21 -0400

    started constructor base

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-alert("sus")
+const myLibrary=[];
+
+
+//book constructor
+function Book(name, author, pages, hasRead){
+    if (!new.target){
+        throw Error("You must use 'new' when using constructor")
+    }
+
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+
+};
+
+function addBookToLib (){
+
+};
+
+let book1 = new Book("book of sus", "sus", 69, true);
+
+console.table(book1)
